e8ccd8d97008fe3190b3d84e7cebcb1216c4297d
Pierre BOURDU <pierre.bourdu@gmail.com>
2016-11-23 18:11:29 +0100

    fix export IPersonne...

--- src/core/models/models.ts
+++ src/core/models/models.ts
@@ -1,7 +1,7 @@
 import { Entreprise, IEntreprise } from './entreprise/entreprise';
 import { Adresse, IAdresse } from './adresse/adresse';
 import { Identification, IIdentification } from './identification/identification';
-import { Personne, PersonnePhysique, PersonneMorale } from './personne/personne';
+import { Personne, PersonnePhysique, PersonneMorale, IPersonne, IPersonneMorale, IPersonnePhysique } from './personne/personne';
 
 export{ 
     Entreprise, IEntreprise,
@@ -9,5 +9,6 @@ export{
     Identification, IIdentification,
     Personne,
     PersonneMorale,
-    PersonnePhysique
+    PersonnePhysique,
+    IPersonne, IPersonneMorale, IPersonnePhysique
 };
