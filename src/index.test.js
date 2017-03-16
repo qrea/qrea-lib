cebb9d814a71a6441540a402f7c6ea1f638f6e09
Pierre BOURDU <pierre.bourdu@gmail.com>
2017-03-16 13:06:47 +0100

    suppr Fisca

--- src/index.test.js
+++ src/index.test.js
@@ -3,22 +3,22 @@ var QreaLib = require('./index');
 
 describe('QreaLib', function () {
 
-    describe('#Fisca', function () {
-    
-        it('doit instancier un calculette', function () {
-            const res = new QreaLib.Fisca.Calculettes.PlusValueImmobiliereCalculette({
-                dateCession: new Date(2016,0,23),
-                dateAcquisition: new Date(2000, 3, 23),
-                prixCession: 340000,
-                prixRevient: 150000
-            });
-            assert.isDefined(res, 'Impossible d\'instancier la calculette PlusValueImmobiliereCalculette');
-        });
+    // describe('#Fisca', function () {
 
-    });
+    //     it('doit instancier un calculette', function () {
+    //         const res = new QreaLib.Fisca.Calculettes.PlusValueImmobiliereCalculette({
+    //             dateCession: new Date(2016,0,23),
+    //             dateAcquisition: new Date(2000, 3, 23),
+    //             prixCession: 340000,
+    //             prixRevient: 150000
+    //         });
+    //         assert.isDefined(res, 'Impossible d\'instancier la calculette PlusValueImmobiliereCalculette');
+    //     });
+
+    // });
 
     describe('#Compta', function () {
-    
+
         it('doit instancier une comptablité', function () {
             const c = new QreaLib.Compta.Models.Comptabilite();
             assert.isDefined(c, 'Impossible d\'instancier une comptabilité');
